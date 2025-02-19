7c2bd8667e048fd12d8d15f135f60a487a58293b
kayrakocaeli <83376559+kayrakocaeli@users.noreply.github.com>
2025-02-19 12:42:52 +0300

    playerdata fix

--- Assets/1_Components/UI/Landing/Landing.cs
+++ Assets/1_Components/UI/Landing/Landing.cs
@@ -9,8 +9,7 @@ public class Landing : MonoBehaviour
     [SerializeField] private Selection feudatorySelection;
     [SerializeField] private Selection avatarSelection;
     [SerializeField] private TMP_InputField userName;
-    //[SerializeField] private Selection emblemSelection;
-
+    // [SerializeField] private Selection emblemSelection;
 
     private void Start()
     {
@@ -19,12 +18,34 @@ public class Landing : MonoBehaviour
 
     private void OnLoginDone()
     {
-        PlayersManager.Instance.playerData.feudetoryId = (feudatorySelection as FeudatorySelection).feudatories[feudatorySelection.Value].Id;
-        PlayersManager.Instance.playerData.avatarId = (avatarSelection as AvatarSelection).avatars[avatarSelection.Value].Id;
-        //PlayersManager.Instance.playerData.emblemId = emblemSelection.Value;
+        var pm = PlayersManager.Instance;
+
+        if (string.IsNullOrEmpty(pm.playerData.playerName))
+        {
+            pm.playerData = new PlayerData(userName.text);
+        }
+        else
+        {
+            pm.playerData.playerName = userName.text;
+        }
+
+        if (pm.playerData.playerLevel == 0)
+        {
+            pm.playerData.playerLevel = 1;
+            pm.playerData.gold = 1000;
+            pm.playerData.food = 500;
+        }
+
+        pm.playerData.feudetoryId = (feudatorySelection as FeudatorySelection).feudatories[feudatorySelection.Value].Id;
+        pm.playerData.avatarId = (avatarSelection as AvatarSelection).avatars[avatarSelection.Value].Id;
 
         LoginManager.Instance.SetUserData(new LoginManager.Data(userName.text));
 
+        string dataJson = JsonUtility.ToJson(pm.playerData);
+        PlayerPrefs.SetString("PlayerPublicData", dataJson);
+        PlayerPrefs.Save();
+
+        //PlayersManager.Instance.playerData.emblemId = emblemSelection.Value;
         //PlayerData.SaveLocal(PlayersManager.Instance.playerData);
 
         ScenesManager.Instance.LoadScene(ScenesManager.Scene.Game);
