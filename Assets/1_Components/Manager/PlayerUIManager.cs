cf4ac64d944b513dbf00ee67720097f56ff236a8
kayrakocaeli <83376559+kayrakocaeli@users.noreply.github.com>
2025-02-24 17:08:42 +0300

    Player stats added

--- Assets/1_Components/Manager/PlayerUIManager.cs
+++ Assets/1_Components/Manager/PlayerUIManager.cs
@@ -1,11 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using ZeroGame;
 
 public class PlayerUIManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text coinText;
     [SerializeField] private TMP_Text gemText;
     [SerializeField] private TMP_Text grainText;
+    [SerializeField] private TMP_Text playerNameText;
+    [SerializeField] private TMP_Text playerLevelText;
+    [SerializeField] private Image avatarImage;
 
     private void Start()
     {
@@ -18,9 +23,18 @@ public class PlayerUIManager : MonoBehaviour
             return;
 
         var data = PlayersManager.Instance.playerData;
-        
-        coinText.text = " " +data.coin;
+
+        coinText.text = " " + data.coin;
         gemText.text = " " + data.gem;
         grainText.text = " " + data.grain;
+
+        playerNameText.text = data.playerName;
+        playerLevelText.text = data.playerLevel.ToString();
+
+        AvatarDataSC avatarData = SCDB.Get<AvatarDataSC>(avatar => avatar.Id == data.avatarId);
+        if (avatarData != null)
+        {
+            avatarImage.sprite = avatarData.Photo;
+        }
     }
 }
